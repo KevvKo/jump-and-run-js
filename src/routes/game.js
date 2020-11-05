e5edaad692e27c409e5fa080b56718dde998b7d2
Kevin Klein <KKlein@intershop.com>
2020-11-05 01:01:37 +0100

    function to scale canvas

--- src/routes/game.js
+++ src/routes/game.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './game.css'
 
 export default function Game(){
+
+
+    useEffect(() => {
+
+        const canvas = document.getElementById('gameBoard')
+        const width = window.innerWidth
+        const height = window.innerHeight
+        const header = document.querySelector('header')
+        
+        canvas.width = width
+        canvas.height = height - header.offsetHeight - 6
+    })
     
+
     return (
         <div className='game'>
-            <canvas></canvas>
+            <canvas id="gameBoard"></canvas>
         </div>
     )
 }
