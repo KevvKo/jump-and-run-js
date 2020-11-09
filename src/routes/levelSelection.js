9f3ebacd44c274c598eb59eb212d00a03cf8e681
Kevin Klein <KKlein@intershop.com>
2020-11-09 23:34:33 +0100

    implement dummy-levels

--- src/routes/levelSelection.js
+++ src/routes/levelSelection.js
@@ -2,9 +2,26 @@ import React from 'react'
 
 import './levelSelection.css'
 
+import Level from '../assets/components/level'
+
 export default function LevelSelection(props){
     return(
         <div className="levelSelection">
+            <div>
+                <Level></Level>
+                <Level></Level>
+                <Level></Level>
+            </div>
+            <div>
+                <Level></Level>
+                <Level></Level>
+                <Level></Level>
+            </div>
+            <div>
+                <Level></Level>
+                <Level></Level>
+                <Level></Level>
+            </div>
         </div>
     )
 }
