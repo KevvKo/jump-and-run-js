37e677978c8b3fa59ee439573a3d2f687cdf41dd
KevvKo <kklein915@gmail.com>
2020-12-03 21:27:11 +0100

    add keysReducer

--- src/assets/store/reducers/keysReducer.js
+++ src/assets/store/reducers/keysReducer.js
@@ -1,4 +1,21 @@
-export default keysReducer(state, action){
+const initialState = {
+    keys: []
+}
 
-    if(action.type === "keys/")
+export default function keysReducer(state = initialState, action){
+
+    if(action.type === "keys/addKey"){
+       
+        state.keys[action.payload] = true 
+
+        return {
+           keys: [ action.key = true, ...state.keys]
+        }
+    } 
+
+    if(action.type === "keys/removeKey"){
+        return state.keys[action.payload] = false
+    }
+
+    return state;
 }
