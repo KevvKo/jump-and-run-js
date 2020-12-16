a1cf69e5e839473aa1983744c0c1dede911578ae
KevvKo <kklein915@gmail.com>
2020-12-16 19:24:26 +0100

    add background-component to guide.js

--- src/routes/guide.js
+++ src/routes/guide.js
@@ -2,8 +2,12 @@ import { render } from '@testing-library/react'
 import React from 'react'
 import './guide.css'
 
+import Background from '../assets/components/background'
+
 export default function Guide(){
     return(
-        <div className='guide'></div>
+        <div className='guide'>
+            <Background />
+        </div>
     )
 }
