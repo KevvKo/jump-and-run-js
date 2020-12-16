ddc74b09eca02efaa9087a59580f0032e8e11ec6
KevvKo <kklein915@gmail.com>
2020-12-16 19:39:47 +0100

    add default size-values to background-component and customized size for guide.js

--- src/assets/components/background.js
+++ src/assets/components/background.js
@@ -3,8 +3,16 @@ import './background.css'
 
 export default function Background(props){
 
+    //default style for the component
+    let size = { width: '40%', height: '70vh' }
+
+    //overwrite the default values
+    if(props.size){
+        size = props.size
+    }
+
     return(
-        <div className="background">
+        <div className="background" style={ size }>
             {props.content}
         </div>
     )
--- src/routes/guide.js
+++ src/routes/guide.js
@@ -5,9 +5,12 @@ import './guide.css'
 import Background from '../assets/components/background'
 
 export default function Guide(){
+
+    const size = { width: "80%", height: "80%" }
+
     return(
         <div className='guide'>
-            <Background />
+            <Background size= { size } />
         </div>
     )
 }
