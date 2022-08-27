c044119a5a1855ca9d95b08b44c3e7889263adb1
KevvKo <kklein915@gmail.com>
2022-08-27 20:47:38 +0200

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,9 @@ module.exports = {
             template: './public/index.html'
         })
     ],
+    devServer: {
+        historyApiFallback: true,
+    },
     module: {
         rules: [
             {
