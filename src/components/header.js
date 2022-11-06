32161f39fb45d8509b12f7145b63ef5ab1304b7e
KevvKo <kklein915@gmail.com>
2022-11-06 17:57:54 +0100

    update pause menu

--- src/components/header.js
+++ src/components/header.js
@@ -1,73 +1,10 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { Link, useLocation } from "react-router-dom";
-import { useSelector } from 'react-redux';
-import { useMenuMusic } from '../scripts/services/music';
-import { store } from '../store/store';
-import { 
-    replaceArtistName, 
-    replaceTitleName, 
-    stopMusic, 
-    playMusic
-} from '../store/actions/musicActions';
-import GameContext from '../providers/GameProvider';
+
 
 function AppHeader() {
 
     const location = useLocation()['pathname'];
-
-    // let index = 0;
-    // const sounds = useMenuMusic();
-    // const [audioElement] = useState( new Audio(sounds[index].file) );
-    // const game = useContext(GameContext);
-
-    // audioElement.onended = () =>{
-  
-    //   index < sounds.length ? index += 1 : index = 0;
-  
-    //   audioElement.src = sounds[index].file;
-    //   audioElement.play();
-  
-    //   const artist = sounds[index].artist;
-    //   const title = sounds[index].track;
-
-    //   store.dispatch(replaceArtistName(artist));
-    //   store.dispatch(replaceTitleName(title));
-    // };
-  
-    // const toggleMenuMusic = () => {
-  
-    //   let button = document.querySelector('.playMusic');
-  
-    //   if(!audioElement.paused){
-  
-    //     audioElement.pause();
-    //     button.children[0].innerText = 'music_off';
-    //     store.dispatch(stopMusic());
-    
-    //   }else{
-    //     const artist = sounds[index].artist;
-    //     const title = sounds[index].track;
-
-    //     audioElement.play();
-    //     button.children[0].innerText = 'music_note';
-    //     store.dispatch(replaceArtistName(artist));
-    //     store.dispatch(replaceTitleName(title));
-    //     store.dispatch(playMusic());
-    //   }
-    // };
-
-    // const togglePauseGame = () => { game.togglePause(); };
-    // const isPaused = useSelector( state => state.game.gameIsPaused);
-    // const showPause = isPaused && location === '/play';
-    // const showContinue = !isPaused && location === '/play';
-
-    // useEffect(() => {
-    //     window.addEventListener('keydown', (e) => {
-    //         if(e.code === 'KeyO'){
-    //             toggleMenuMusic();
-    //         }
-    //     });
-    // }, [toggleMenuMusic]);
 
     return(
         <header className='absolute top-0 p-2.5 z-10 w-full'>
--- src/routes/play.js
+++ src/routes/play.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate, Link } from "react-router-dom";
 import Dialog from '../components/dialog';
@@ -7,16 +7,78 @@ import { addKeyDown, addKeyUp } from '../store/actions/keyActions';
 import { resetGame } from '../store/actions/gameActions';
 import { scaleCanvas } from '../store/actions/canvasActions';
 import GameContext from '../providers/GameProvider';
+import { useMenuMusic } from '../scripts/services/music';
+import { 
+    replaceArtistName, 
+    replaceTitleName, 
+    stopMusic, 
+    playMusic
+} from '../store/actions/musicActions';
 
 const styles = {
-    button: 'mr-3 mb-3 bg-slate-900 py-2 px-4 active:bg-slate-600 hover:bg-slate-800 rounded',
-    highscore: 'text-center mb-5 text-2xl', 
-    container: 'flex justify-center flex-col items-center',
-    title: 'text-4xl text-center p-5',
+    button1: 'mr-3 mb-3 bg-slate-900 py-2 px-4 active:bg-slate-600 hover:bg-slate-800 rounded',
+    button2: 'mb-3 bg-slate-900 py-2 px-4 active:bg-slate-600 hover:bg-slate-800 rounded text-center',
     buttonBar: 'mt-7',
+    container: 'flex justify-center flex-col items-center',
+    dialogContainer: 'mx-6 px-8 flex flex-col',
+    highscore: 'text-center mb-5 text-2xl', 
+    title: 'text-4xl text-center p-5',
 };
 
 const Play = () => {    
+    // let index = 0;
+    // const sounds = useMenuMusic();
+    // const [audioElement] = useState( new Audio(sounds[index].file) );
+    // const game = useContext(GameContext);
+
+    // audioElement.onended = () =>{
+  
+    //   index < sounds.length ? index += 1 : index = 0;
+  
+    //   audioElement.src = sounds[index].file;
+    //   audioElement.play();
+  
+    //   const artist = sounds[index].artist;
+    //   const title = sounds[index].track;
+
+    //   store.dispatch(replaceArtistName(artist));
+    //   store.dispatch(replaceTitleName(title));
+    // };
+  
+    // const toggleMenuMusic = () => {
+  
+    //   let button = document.querySelector('.playMusic');
+  
+    //   if(!audioElement.paused){
+  
+    //     audioElement.pause();
+    //     button.children[0].innerText = 'music_off';
+    //     store.dispatch(stopMusic());
+    
+    //   }else{
+    //     const artist = sounds[index].artist;
+    //     const title = sounds[index].track;
+
+    //     audioElement.play();
+    //     button.children[0].innerText = 'music_note';
+    //     store.dispatch(replaceArtistName(artist));
+    //     store.dispatch(replaceTitleName(title));
+    //     store.dispatch(playMusic());
+    //   }
+    // };
+
+    // const togglePauseGame = () => { game.togglePause(); };
+    // const isPaused = useSelector( state => state.game.gameIsPaused);
+    // const showPause = isPaused && location === '/play';
+    // const showContinue = !isPaused && location === '/play';
+
+    // useEffect(() => {
+    //     window.addEventListener('keydown', (e) => {
+    //         if(e.code === 'KeyO'){
+    //             toggleMenuMusic();
+    //         }
+    //     });
+    // }, [toggleMenuMusic]);
 
     const game = useContext(GameContext);
     const navigate = useNavigate();
@@ -71,10 +133,13 @@ const Play = () => {
         <div className={`play ${styles.container}`} >
             { gameIsPaused &&
                 <Dialog>
-                    <h3 className={styles.title}>Game Paused</h3>
-                    <Link className={styles.button} to='/'>Menu</Link>
-                    <Link className={styles.button} to='/chooseDifficulty'>Difficulty Selection</Link>
-                    <button className={styles.button} onClick={handleClickRestart}>Restart</button>
+                    <div className={styles.dialogContainer}>
+                        <h3 className={styles.title}>Game Paused</h3>
+                        <Link className={styles.button2} to='/'>Back to Menu</Link>
+                        <Link className={styles.button2} to='/chooseDifficulty'>Difficulty Selection</Link>
+                        <button className={styles.button2} onClick={handleClickRestart}>Restart Game</button>
+                        <button className={styles.button2} onClick={handleClickRestart}>Sound on/off</button>
+                    </div>
                 </Dialog> 
             }
             { gameIsOver &&
@@ -83,10 +148,10 @@ const Play = () => {
                         <div className={styles.title}>Game Over</div>
                         <div className={styles.highscore}>Punkte: {game.highscore}</div>
                         <div className={styles.buttonBar}>
-                            <button  className={styles.button} onClick={handleClickGoHome}>
+                            <button  className={styles.button1} onClick={handleClickGoHome}>
                                 Zur Startseite
                             </button>
-                            <button className={styles.button} onClick={handleClickRestart}>
+                            <button className={styles.button1} onClick={handleClickRestart}>
                                 Neuer Versuch
                             </button>
                         </div>
